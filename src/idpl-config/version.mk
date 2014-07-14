da01bb53aa98303a4c4b741b79c5e0c518f0fa23
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2014-07-14 14:43:20 -0700

    Initial Checkin

--- src/idpl-config/version.mk
+++ src/idpl-config/version.mk
@@ -1,5 +1,5 @@
 NAME        = idpl-config
 VERSION     = 1.0
 RELEASE     = 1
+RPM.FILE.EXTRAS = "%conf /etc/idpl/idpl.sysctl"
 RPM.FILESLIST = idpl.files
-RPM.FILE.EXTRAS ="%conf /etc/idpl/idpl.sysctl"
